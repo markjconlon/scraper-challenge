a18836f36c74e0aadf9cfaa08c80164198ef84b4
markjconlon <markjconlon@gmail.com>
2017-08-25 11:18:53 -0400

    refactored

--- meetup.rb
+++ meetup.rb
@@ -2,17 +2,21 @@ require 'Nokogiri'
 require 'open-uri'
 require 'csv'
 
-start_url = "https://www.meetup.com/fintechto/members/?offset="
-offset = "0"
-end_url = "&sort=name&desc=0"
-page = Nokogiri::HTML(open(start_url + offset + end_url))
-mem_list = page.css("#member_list h4>a")
-counter = 0
+def scrape_page(num_pages)
+  start_url = "https://www.meetup.com/fintechto/members/?offset="
+  offset = ((num_pages - 1) * 20).to_s
+  end_url = "&sort=name&desc=0"
+  page = Nokogiri::HTML(open(start_url + offset + end_url))
+  mem_list = page.css("#member_list h4>a")
+  counter = 0
 
-mem_list.each do |li|
-  id = (mem_list[counter]["href"]).split.map {|char| char.gsub(/[^\d]/, '')}
-  CSV.open('names_ids.csv', 'a+') do |csv|
-    csv << [mem_list[counter].text, id.join]
+  mem_list.each do |li|
+    id = (mem_list[counter]["href"]).split.map {|char| char.gsub(/[^\d]/, '')}
+    CSV.open('names_ids.csv', 'a+') do |csv|
+      csv << [mem_list[counter].text, id.join]
+    end
+    counter += 1
   end
-  counter += 1
 end
+
+scrape_page(1)
