eb38268cc1f190c8730603882556ee27371c44c7
markjconlon <markjconlon@gmail.com>
2017-08-25 11:44:14 -0400

    annotated code

--- meetup.rb
+++ meetup.rb
@@ -1,7 +1,12 @@
+# both of the sites do not display emails probably for this very reason
+# of scraping. Meetup used to however they now encourage in app messaging
+# therefore I chose to grab member ids
 require 'Nokogiri'
 require 'open-uri'
 require 'csv'
 
+# takes the data passed from get_page and appends desired pieces
+# to names_ids.csv
 def scrape_page(mem_list)
   counter = 0
   mem_list.each do |li|
@@ -13,9 +18,14 @@ def scrape_page(mem_list)
   end
 end
 
+# get_page simply does that it gets the data from the page we
+# want and then sends it for further extraction and appending
+# via scrape_page
 def get_page(num_pages)
   counter = 0
   num_pages.times do
+    # split the url up for readability and allows easy subustitution
+    # if we would like to scrape other groups in the future
     start_url = "https://www.meetup.com/fintechto/members/?offset="
     offset = (counter * 20).to_s
     end_url = "&sort=name&desc=0"
@@ -23,6 +33,8 @@ def get_page(num_pages)
     mem_list = page.css("#member_list h4>a")
     scrape_page(mem_list)
     counter += 1
+    # added a short random sleep timer in order to lower the frequecy
+    # of requests to meetups servers
     sleep rand(1..3)
   end
 end
