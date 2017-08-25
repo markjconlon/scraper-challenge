2e8d6d27d9cc1bcad48edc5bdbbbabe45344d37c
markjconlon <markjconlon@gmail.com>
2017-08-25 11:33:33 -0400

    split into two functions one to retrieve the data of each page and one to extract names and ids of each member and append to the csv file named get_page and scrape_page respectively

--- meetup.rb
+++ meetup.rb
@@ -2,14 +2,8 @@ require 'Nokogiri'
 require 'open-uri'
 require 'csv'
 
-def scrape_page(num_pages)
-  start_url = "https://www.meetup.com/fintechto/members/?offset="
-  offset = ((num_pages - 1) * 20).to_s
-  end_url = "&sort=name&desc=0"
-  page = Nokogiri::HTML(open(start_url + offset + end_url))
-  mem_list = page.css("#member_list h4>a")
+def scrape_page(mem_list)
   counter = 0
-
   mem_list.each do |li|
     id = (mem_list[counter]["href"]).split.map {|char| char.gsub(/[^\d]/, '')}
     CSV.open('names_ids.csv', 'a+') do |csv|
@@ -19,4 +13,19 @@ def scrape_page(num_pages)
   end
 end
 
-scrape_page(1)
+def get_page(num_pages)
+  counter = 0
+  num_pages.times do
+    start_url = "https://www.meetup.com/fintechto/members/?offset="
+    offset = (counter * 20).to_s
+    end_url = "&sort=name&desc=0"
+    page = Nokogiri::HTML(open(start_url + offset + end_url))
+    mem_list = page.css("#member_list h4>a")
+    scrape_page(mem_list)
+    counter += 1
+    sleep rand(1..3)
+  end
+end
+
+# Meetup displays 20 results per page 20*25 pages = 500 names
+get_page(25)
